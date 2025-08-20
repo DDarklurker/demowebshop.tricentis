2ca3c33f1c70e572cd038f69a184a3662bc0ac87
DDarklurker <foxover.rit@gmail.com>
2025-08-20 18:44:27 +0300

    add prettier

--- pages/register/registerPage.ts
+++ pages/register/registerPage.ts
@@ -1,25 +1,25 @@
 import { Locator, Page } from "@playwright/test";
 import { BasePage } from "../base/basePage";
 
-export class RegisterPage extends BasePage{
-    readonly genderManePoint: Locator;
-    readonly genderFemalePoint: Locator;
-    readonly firstNamePlaceholder: Locator;
-    readonly lastNamePlaceholder: Locator;
-    readonly emailPlaceholder: Locator;
-    readonly passwordPlaceholder: Locator;
-    readonly confirmPasswordPlaceholder: Locator;
-    readonly registerButton: Locator;
+export class RegisterPage extends BasePage {
+  readonly genderManePoint: Locator;
+  readonly genderFemalePoint: Locator;
+  readonly firstNamePlaceholder: Locator;
+  readonly lastNamePlaceholder: Locator;
+  readonly emailPlaceholder: Locator;
+  readonly passwordPlaceholder: Locator;
+  readonly confirmPasswordPlaceholder: Locator;
+  readonly registerButton: Locator;
 
-    constructor(page:Page) {
-        super(page);
-        this.genderManePoint = page.locator('#gender-male');
-        this.genderFemalePoint = page.locator('#gender-female');
-        this.firstNamePlaceholder = page.locator('#FirstName');
-        this.lastNamePlaceholder = page.locator('#LastName');
-        this.emailPlaceholder = page.locator('#Email');
-        this.passwordPlaceholder = page.locator('#Password');
-        this.confirmPasswordPlaceholder = page.locator('#ConfirmPassword');
-        this.registerButton = page.locator('#register-button');
-    }
+  constructor(page: Page) {
+    super(page);
+    this.genderManePoint = page.locator("#gender-male");
+    this.genderFemalePoint = page.locator("#gender-female");
+    this.firstNamePlaceholder = page.locator("#FirstName");
+    this.lastNamePlaceholder = page.locator("#LastName");
+    this.emailPlaceholder = page.locator("#Email");
+    this.passwordPlaceholder = page.locator("#Password");
+    this.confirmPasswordPlaceholder = page.locator("#ConfirmPassword");
+    this.registerButton = page.locator("#register-button");
+  }
 }
--- tests/tests/authorization/login.spec.ts
+++ tests/tests/authorization/login.spec.ts
@@ -1,36 +1,57 @@
-import { test, expect } from '../../../fixture/fixturePage';
-import { faker } from '@faker-js/faker';
+import { test, expect } from "../../../fixture/fixturePage";
+import { faker } from "@faker-js/faker";
 
-
-test.describe('Login Tests: @authorization', () =>{
-    test.beforeEach(async({page, basePage})=>{
-        await page.goto('/');
-        await basePage.verifyBasePage();
-        await basePage.loginTab.click();
-        await expect(page).toHaveURL('/login');
-    });
-    test('Test Case 1: Login User with correct email and password.', {tag: '@smoke'}, async ({basePage, loginPage}) => {
-        await loginPage.emailPlaceholder.fill(process.env.LOGIN as string);
-        await loginPage.passwordPlaceholder.fill(process.env.PASSWORD as string);
-        await loginPage.loginButton.click();
-        await expect(basePage.customerInfoTab).toContainText(process.env.LOGIN as string);
-        await basePage.logoutTab.click();
-        await expect(basePage.customerInfoTab).not.toContainText(process.env.LOGIN as string);
-    });
-    test('Test Case 2: Login User with incorrect password.',  {tag: '@smoke'}, async ({loginPage}) => {
-        await loginPage.emailPlaceholder.fill(process.env.LOGIN as string);
-        await loginPage.passwordPlaceholder.fill(faker.internet.password());
-        await loginPage.loginButton.click();
-        await expect(loginPage.incorrectLoginMessage).toContainText('The credentials provided are incorrect');
-    });
-    test('Test Case 3:  Login User with incorrect email.', async ({loginPage}) => {
-        await loginPage.emailPlaceholder.fill(faker.internet.email()); 
-        await loginPage.loginButton.click();
-        await expect(loginPage.incorrectLoginMessage).toContainText('No customer account found');
-    });
-    test('Test Case 4:  Login User with invalid email.', async ({loginPage}) => {
-        await loginPage.emailPlaceholder.fill(faker.internet.username());
-        await loginPage.loginButton.click();
-        await expect(loginPage.validationEmailField).toContainText('Please enter a valid email address.');
-    });
+test.describe("Login Tests: @authorization", () => {
+  test.beforeEach(async ({ page, basePage }) => {
+    await page.goto("/");
+    await basePage.verifyBasePage();
+    await basePage.loginTab.click();
+    await expect(page).toHaveURL("/login");
+  });
+  test(
+    "Test Case 1: Login User with correct email and password.",
+    { tag: "@smoke" },
+    async ({ basePage, loginPage }) => {
+      await loginPage.emailPlaceholder.fill(process.env.LOGIN as string);
+      await loginPage.passwordPlaceholder.fill(process.env.PASSWORD as string);
+      await loginPage.loginButton.click();
+      await expect(basePage.customerInfoTab).toContainText(
+        process.env.LOGIN as string
+      );
+      await basePage.logoutTab.click();
+      await expect(basePage.customerInfoTab).not.toContainText(
+        process.env.LOGIN as string
+      );
+    }
+  );
+  test(
+    "Test Case 2: Login User with incorrect password.",
+    { tag: "@smoke" },
+    async ({ loginPage }) => {
+      await loginPage.emailPlaceholder.fill(process.env.LOGIN as string);
+      await loginPage.passwordPlaceholder.fill(faker.internet.password());
+      await loginPage.loginButton.click();
+      await expect(loginPage.incorrectLoginMessage).toContainText(
+        "The credentials provided are incorrect"
+      );
+    }
+  );
+  test("Test Case 3:  Login User with incorrect email.", async ({
+    loginPage,
+  }) => {
+    await loginPage.emailPlaceholder.fill(faker.internet.email());
+    await loginPage.loginButton.click();
+    await expect(loginPage.incorrectLoginMessage).toContainText(
+      "No customer account found"
+    );
+  });
+  test("Test Case 4:  Login User with invalid email.", async ({
+    loginPage,
+  }) => {
+    await loginPage.emailPlaceholder.fill(faker.internet.username());
+    await loginPage.loginButton.click();
+    await expect(loginPage.validationEmailField).toContainText(
+      "Please enter a valid email address."
+    );
+  });
 });
