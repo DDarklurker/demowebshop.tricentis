cb426e24026a3ffb15b8c148b4159aa07f773fb9
DDarklurker <foxover.rit@gmail.com>
2025-08-27 19:40:38 +0300

    fix import

--- src/pages/cart/cartPage.ts
+++ src/pages/cart/cartPage.ts
@@ -1,5 +1,5 @@
 import { Locator, Page } from "@playwright/test";
-import { BasePage } from "../base/basePage";
+import { BasePage } from "../basePage";
 
 export class CartPage extends BasePage {
   readonly checkoutButton: Locator;
