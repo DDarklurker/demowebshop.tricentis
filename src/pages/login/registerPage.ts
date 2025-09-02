e275e3da5150f9495e2a581f01ad456cff09cb17
DDarklurker <foxover.rit@gmail.com>
2025-09-02 20:24:51 +0300

    Implement registration functionality in RegisterPage with gender selection and input validation

--- src/pages/login/registerPage.ts
+++ src/pages/login/registerPage.ts
@@ -1,6 +1,7 @@
-import { Locator } from "@playwright/test";
+import { expect, Locator } from "@playwright/test";
 import { BasePage } from "../basePage";
 
+export type sexType = "male" | "female";
 export class RegisterPage extends BasePage {
   readonly genderManePoint: Locator;
   readonly genderFemalePoint: Locator;
@@ -21,5 +22,59 @@ export class RegisterPage extends BasePage {
     this.passwordPlaceholder = page.locator("#Password");
     this.confirmPasswordPlaceholder = page.locator("#ConfirmPassword");
     this.registerButton = page.locator("#register-button");
+  }
+  async clickGenderPoint(sex: sexType) {
+    if (sex === "male") {
+      await this.genderManePoint.click();
+    } else {
+      await this.genderFemalePoint.click();
+    }
+  }
+  async enterFirstName(firstName: string) {
+    await this.firstNamePlaceholder.fill(firstName);
+    await expect(this.firstNamePlaceholder).toHaveValue(firstName);
+  }
+  async enterLastName(lastName: string) {
+    await this.lastNamePlaceholder.fill(lastName);
+    await expect(this.lastNamePlaceholder).toHaveValue(lastName);
+  }
+  async enterEmail(email: string) {
+    await this.emailPlaceholder.fill(email);
+    await expect(this.emailPlaceholder).toHaveValue(email);
+  }
+  async enterPassword(password: string) {
+    await this.passwordPlaceholder.fill(password);
+    await expect(this.passwordPlaceholder).toHaveValue(password);
+  }
+  async enterConfirmPassword(confirmPassword: string) {
+    await this.confirmPasswordPlaceholder.fill(confirmPassword);
+    await expect(this.confirmPasswordPlaceholder).toHaveValue(confirmPassword);
+  }
+  async clickRegisterButton() {
+    await this.registerButton.click();
+  }
+  async register(
+    sex: sexType,
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
+    confirmPassword: string
+  ) {
+    await this.clickGenderPoint(sex);
+    await this.enterFirstName(firstName);
+    await this.enterLastName(lastName);
+    await this.enterEmail(email);
+    await this.enterPassword(password);
+    await this.enterConfirmPassword(confirmPassword);
+    await this.clickRegisterButton();
+  }
+  async verifyElements() {
+    await expect(this.genderManePoint).toBeVisible();
+    await expect(this.genderFemalePoint).toBeVisible();
+    await expect(this.firstNamePlaceholder).toBeVisible();
+    await expect(this.lastNamePlaceholder).toBeVisible();
+    await expect(this.emailPlaceholder).toBeVisible();
+    await expect(this.passwordPlaceholder).toBeVisible();
   }
 }
