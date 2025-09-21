ec4079f4be143ed95f60bccce89ec0e12d67d771
DDarklurker <foxover.rit@gmail.com>
2025-09-21 08:39:39 +0300

    Remove README and GitHub Actions workflows for testing; update Playwright configuration to conditionally set reporter based on CI environment.

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["blob", { outputDir: "test-results" }]],
+  reporter: process.env.CI
+    ? [["blob", { outputDir: "test-results" }]]
+    : [["html", { outputDir: "test-results" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
