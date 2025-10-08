7f69eb451a41bdc19d2c6b6a809b333a0130d0d6
DDarklurker <foxover.rit@gmail.com>
2025-10-08 14:19:58 +0300

    fix abstract class

--- setup/global-setup.ts
+++ setup/global-setup.ts
@@ -1,22 +1,22 @@
-import { chromium, type FullConfig } from "@playwright/test";
-import { LoginPage } from "../src/pages/login/loginPage";
-import { BasePage } from "../src/pages/basePage";
+import { chromium, type FullConfig } from '@playwright/test';
+import { LoginPage } from '../src/pages/login/loginPage';
+import { HomePage } from '../src/pages/basePage';
 
 async function globalSetup(config: FullConfig) {
-  const { baseURL, storageState } = config.projects[0].use;
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
-  const basePage = new BasePage(page);
-  const loginPage = new LoginPage(page);
-  await page.goto(baseURL!);
-  await basePage.loginTab.click();
-  await page.waitForURL("**/login");
-  await loginPage.emailPlaceholder.fill(process.env.LOGIN as string);
-  await loginPage.passwordPlaceholder.fill(process.env.PASSWORD as string);
-  await loginPage.loginButton.click();
-  await page.waitForURL(baseURL!);
-  await page.context().storageState({ path: storageState as string });
-  await browser.close();
+    const { baseURL, storageState } = config.projects[0].use;
+    const browser = await chromium.launch();
+    const page = await browser.newPage();
+    const homePage = new HomePage(page);
+    const loginPage = new LoginPage(page);
+    await page.goto(baseURL!);
+    await homePage.headerComponent.loginTab.click();
+    await page.waitForURL('**/login');
+    await loginPage.emailPlaceholder.fill(process.env.LOGIN as string);
+    await loginPage.passwordPlaceholder.fill(process.env.PASSWORD as string);
+    await loginPage.loginButton.click();
+    await page.waitForURL(baseURL!);
+    await page.context().storageState({ path: storageState as string });
+    await browser.close();
 }
 
 export default globalSetup;
