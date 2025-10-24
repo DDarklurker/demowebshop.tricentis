09f3885af6563d87c0e41f771a0f4cfb32864545
DDarklurker <foxover.rit@gmail.com>
2025-10-24 11:58:46 +0300

    Refactor authorization tests to improve structure and consistency by updating import statements and utilizing the app context for page interactions. Enhance readability and maintainability across login, recover, and register test files.

--- tests/tests/authorization/recover.spec.ts
+++ tests/tests/authorization/recover.spec.ts
@@ -1,18 +1,19 @@
 import { faker } from "@faker-js/faker";
-import { test, expect } from "../../../src/fixture/fixturePage";
+import { test } from "../../../src/fixture/fixturePage";
 import pagesUrl from "../../../src/utils/pagesUrl";
+import { expect } from "@playwright/test";
 
 const login = process.env.LOGIN as string;
 test.describe("Recover Tests: @authorization", async () => {
-  test.beforeEach(async ({ page, basePage, loginPage }) => {
-    await page.goto(pagesUrl.home);
-    await basePage.headerComponent.clickLoginTab();
+  test.beforeEach(async ({ app: { homePage, loginPage } }) => {
+    await homePage.open(pagesUrl.home);
+    await homePage.headerComponent.clickLoginTab();
     await loginPage.clickForgotPassword();
   });
   test(
     "Test Case 1: Recovere with correct email.",
     { tag: "@smoke" },
-    async ({ recoverPage }) => {
+    async ({ app: { recoverPage } }) => {
       await recoverPage.recoverLogin(login, pagesUrl.recover);
       await expect(recoverPage.resultMessage).toContainText(
         "Email with instructions has been sent to you."
@@ -20,22 +21,28 @@ test.describe("Recover Tests: @authorization", async () => {
     }
   );
   test("Test Case 2: Recovere with incorrect email.", async ({
-    recoverPage,
+    app: { recoverPage }
   }) => {
     await recoverPage.recoverLogin(faker.internet.email(), pagesUrl.recover);
     await expect(recoverPage.resultMessage).toContainText("Email not found.");
   });
-  test("Test Case 3: Recovere with invalid email.", async ({ recoverPage }) => {
+  test("Test Case 3: Recovere with invalid email.", async ({
+    app: { recoverPage }
+  }) => {
     await recoverPage.recoverLogin(faker.internet.username(), pagesUrl.recover);
     await expect(recoverPage.validationMessage).toContainText("Wrong email");
   });
-  test("Test Case 4: Recovere with empty email.", async ({ recoverPage }) => {
+  test("Test Case 4: Recovere with empty email.", async ({
+    app: { recoverPage }
+  }) => {
     await recoverPage.clickRecoverButton();
     await expect(recoverPage.validationMessage).toContainText(
       "Enter your email"
     );
   });
-  test("Test Case 5: verify UI elements on page", async ({ recoverPage }) => {
+  test("Test Case 5: verify UI elements on page", async ({
+    app: { recoverPage }
+  }) => {
     await expect(recoverPage.emailPlaceholder).toBeVisible();
     await expect(recoverPage.recoverButton).toBeVisible();
   });
--- tests/tests/authorization/register.spec.ts
+++ tests/tests/authorization/register.spec.ts
@@ -1,19 +1,22 @@
-import { test, expect } from "../../../src/fixture/fixturePage";
+import { expect } from "@playwright/test";
+import { test } from "../../../src/fixture/fixturePage";
 import { genderType } from "../../../src/pages/login/registerPage";
 import pagesUrl from "../../../src/utils/pagesUrl";
 import users from "../../data/users.json";
 import { faker } from "@faker-js/faker";
 const data = users[2];
 test.describe("Register", () => {
-  test.beforeEach(async ({ page, basePage }) => {
-    await page.goto(pagesUrl.home);
-    await basePage.headerComponent.clickRegisterTab();
+  test.beforeEach(async ({ app: { homePage } }) => {
+    await homePage.open(pagesUrl.home);
+    await homePage.headerComponent.clickRegisterTab();
   });
-  test("Verify UI elements on page", async ({ registerPage }) => {
+  test("Verify UI elements on page", async ({ app: { registerPage } }) => {
     await registerPage.verifyElements();
   });
 
-  test("@validation: Password ≠ Confirm password", async ({ registerPage }) => {
+  test("@validation: Password ≠ Confirm password", async ({
+    app: { registerPage }
+  }) => {
     await registerPage.registerUser(
       data.gender as genderType,
       data.firstname,
@@ -27,7 +30,9 @@ test.describe("Register", () => {
       "The password and confirmation password do not match."
     );
   });
-  test("@regression: Email already exists", async ({ registerPage }) => {
+  test("@regression: Email already exists", async ({
+    app: { registerPage }
+  }) => {
     await registerPage.registerUser(
       data.gender as genderType,
       data.firstname,
@@ -43,8 +48,8 @@ test.describe("Register", () => {
   });
 
   test("@validation: Empty fields -> required errors", async ({
-    page,
-    registerPage
+    app: { registerPage },
+    page
   }) => {
     await registerPage.clickRegisterButton();
     await expect(page).toHaveURL(pagesUrl.register);
@@ -59,7 +64,7 @@ test.describe("Register", () => {
 
   for (const { email, description } of invalidEmails) {
     test(`@validation: Invalid email formats - ${description}`, async ({
-      registerPage
+      app: { registerPage }
     }) => {
       await registerPage.enterEmail(email);
       await registerPage.clickRegisterButton();
@@ -77,7 +82,7 @@ test.describe("Register", () => {
     confirmpassword
   } of users) {
     test(`@smoke: Successfully registration (parameterized) - ${id}`, async ({
-      registerPage
+      app: { registerPage }
     }) => {
       await registerPage.registerUser(
         gender as genderType,
@@ -90,27 +95,31 @@ test.describe("Register", () => {
     });
   }
   test("@functional: Gender radio buttons mutual exclusivity - Male selected", async ({
-    registerPage
+    app: { registerPage }
   }) => {
     await registerPage.genderManePoint.click();
     await expect(registerPage.genderManePoint).toBeChecked();
     await expect(registerPage.genderFemalePoint).not.toBeChecked();
   });
   test("@functional: Gender radio buttons mutual exclusivity - Female selected", async ({
-    registerPage
+    app: { registerPage }
   }) => {
     await registerPage.genderFemalePoint.click();
     await expect(registerPage.genderFemalePoint).toBeChecked();
     await expect(registerPage.genderManePoint).not.toBeChecked();
   });
-  test("@functional: Password field is hidden", async ({ registerPage }) => {
+  test("@functional: Password field is hidden", async ({
+    app: { registerPage }
+  }) => {
     await registerPage.passwordPlaceholder.fill(data.password);
     await expect(registerPage.passwordPlaceholder).toHaveAttribute(
       "type",
       "password"
     );
   });
-  test("@security: Password strength validation", async ({ registerPage }) => {
+  test("@security: Password strength validation", async ({
+    app: { registerPage }
+  }) => {
     await registerPage.registerUser(
       data.gender as genderType,
       data.firstname,
@@ -125,8 +134,8 @@ test.describe("Register", () => {
     );
   });
   test.only("@mock: Successful registration", async ({
-    page,
-    registerPage
+    app: { registerPage },
+    page
   }) => {
     await page.route("**/register", async (route) => {
       await route.fulfill({
